ed8928aa6396678931acf51d33d3314505bf3f64
diegoat1 <53674274+diegoat1@users.noreply.github.com>
2023-11-15 10:17:14 -0300

    2023.11.15

--- src/main.py
+++ src/main.py
@@ -301,7 +301,13 @@ def dashboard():
     
     solver_category=dinamicodata[-1][36]
     try:
-        habitperformance=round(idealdays/dinamicodata[-1][29]*100)
+        if solver_category =="General":
+            habitperformance=round(idealdays/dinamicodata[-1][29]*100)/3
+        elif solver_category =="Positivo":
+            habitperformance=100/3+round(idealdays/dinamicodata[-1][29]*100)/3
+        elif solver_category =="Completo":
+            habitperformance=200/3+round(idealdays/dinamicodata[-1][29]*100)/3
+            
         if habitperformance>=100:
             habitperformance=100
         else:
